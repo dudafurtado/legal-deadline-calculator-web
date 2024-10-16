3f75e686c67d76a11dc7b40dbda0461df2603f64
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-10-16 16:55:26 -0300

    ft: new table and content to holiday page

--- src/app/feriados/page.tsx
+++ src/app/feriados/page.tsx
@@ -1,25 +1,38 @@
+import Menu from "@/components/Menu";
 import { Modal } from "@/components/Modal";
 import { PaginationTable } from "@/components/Pagination";
-import { SelectInput } from "@/components/SelectInput";
+import { SelectInput } from "@/components/SelectState";
 import { TableHolidays } from "@/components/TableHolidays";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function Holidays() {
   return (
-    <article className="m-8">
-      <h1>Feriados</h1>
-      <section className="flex w-full max-w-sm items-center space-x-2">
-        <div className="w-max flex gap-2">
-          <SelectInput />
+    <article className="flex flex-col gap-8 m-8">
+      <Menu />
+
+      <main className="flex flex-col items-center justify-center gap-8">
+        <section className="w-full flex justify-between">
+          <h1 className="text-2xl font-bold">Feriados</h1>
+          <Modal />
+        </section>
+
+        <p className="text-justify">
+          Essa listagem contém por padrão todos os feriados nacionais e
+          estaduais. Já em relação aos municipais estão separados a partir dos
+          10 mais utilizados para consultas de prazos. Caso queira editar o
+          conteúdo do feriado ou deletar clique nos botões da tabela.
+        </p>
+
+        <section className="w-full flex justify-between gap-4">
+          <Input type="text" placeholder="Escreva o nome de um feriado" />
           <SelectInput />
           <Button type="submit">Pesquisar</Button>
-        </div>
-        <Modal />
-      </section>
+        </section>
 
-      <TableHolidays />
-      <PaginationTable />
+        <TableHolidays />
+        <PaginationTable />
+      </main>
     </article>
   );
 }
--- src/components/TableHolidays/index.tsx
+++ src/components/TableHolidays/index.tsx
@@ -1,87 +1,50 @@
+"use client";
+
 import {
   Table,
   TableBody,
   TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+import { Holiday } from "@/interfaces/interfaces";
+import { listHolidays } from "@/services/holidayRequests";
+import { useEffect, useState } from "react";
 
 export function TableHolidays() {
+  const [holidays, setHolidays] = useState<Holiday[]>([]);
+
+  useEffect(() => {
+    async function loadHolidays() {
+      const res = await listHolidays();
+
+      setHolidays(res);
+    }
+
+    loadHolidays();
+  }, []);
+
   return (
     <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableCaption>Listagem dos feriados cadastrados no sistema.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
+          <TableHead>Nome</TableHead>
+          <TableHead>Data</TableHead>
+          <TableHead className="text-right">Estado</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+        {holidays.map((holiday) => (
+          <TableRow key={holiday.id}>
+            <TableCell className="font-medium">{holiday.name}</TableCell>
+            <TableCell>{holiday.date}</TableCell>
+            <TableCell className="text-right">{holiday.state_id}</TableCell>
           </TableRow>
         ))}
       </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
     </Table>
   );
 }
