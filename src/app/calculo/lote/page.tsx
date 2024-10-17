8ea7562c83c0ccd1926c6a6efcce6c8a889f401a
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-10-17 11:33:59 -0300

    fix: styled batch page content

--- src/app/calculo/lote/page.tsx
+++ src/app/calculo/lote/page.tsx
@@ -7,17 +7,24 @@ export default function BatchCalculator() {
   return (
     <article className="flex flex-col gap-8 m-8">
       <Menu />
-      <main className=" flex flex-col items-center justify-center gap-8">
-        <h1 className="text-2xl font-bold mb-2">Cálculo em Lote de Prazos:</h1>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="picture" className="font-semibold">
-            Arquivo Excel
-          </Label>
-          <Input id="picture" type="file" />
-        </div>
-        <Button type="submit" className="w-5/12">
-          Calcular
-        </Button>
+
+      <main className="w-full flex items-center justify-center">
+        <section className="lg:w-4/12 sm:w-7/12 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-2xl font-bold mb-2">
+            Cálculo em Lote de Prazos:
+          </h1>
+
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="picture" className="font-semibold">
+              Arquivo Excel
+            </Label>
+            <Input id="picture" type="file" />
+          </div>
+
+          <Button type="submit" className="w-full">
+            Calcular
+          </Button>
+        </section>
       </main>
     </article>
   );
