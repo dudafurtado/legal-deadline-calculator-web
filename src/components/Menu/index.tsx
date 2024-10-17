64c632f5378197dce5d325937e4116e7469caa20
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-10-17 12:03:43 -0300

    ft: out button at menu bar

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,6 +7,8 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
+  SheetFooter,
+  SheetClose,
 } from "@/components/ui/sheet";
 import Image from "next/image";
 import MenuIcon from "@/assets/menu-icon.png";
@@ -21,6 +23,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function Menu() {
   const router = useRouter();
@@ -33,48 +36,58 @@ export default function Menu() {
       <SheetContent side={"left"}>
         <SheetHeader>
           <SheetTitle>Menu</SheetTitle>
-          <SheetDescription>
-            <Accordion type="single" collapsible>
-              <AccordionItem value="item-1">
-                <AccordionTrigger>Cálculos</AccordionTrigger>
-                <AccordionContent
-                  className="flex items-center gap-2 cursor-pointer"
-                  onClick={() => router.push("/calculo/individual")}
-                >
-                  Prazos Individuais
-                  <Image src={Calculator} width={30} height={30} alt={""} />
-                </AccordionContent>
-                <AccordionContent
-                  className="flex items-center gap-2 cursor-pointer"
-                  onClick={() => router.push("/calculo/lote")}
-                >
-                  Prazos em Lote
-                  <Image src={SheetIcon} width={30} height={30} alt={""} />
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-2">
-                <AccordionTrigger>Prazos</AccordionTrigger>
-                <AccordionContent
-                  className="flex items-center gap-2 cursor-pointer"
-                  onClick={() => router.push("/prazos")}
-                >
-                  Histórico.
-                  <Image src={DeadlineIcon} width={30} height={30} alt={""} />
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-3">
-                <AccordionTrigger>Feriados</AccordionTrigger>
-                <AccordionContent
-                  className="flex items-center gap-2 cursor-pointer"
-                  onClick={() => router.push("/feriados")}
-                >
-                  Listagem e Criação.
-                  <Image src={HolidayIcon} width={30} height={30} alt={""} />
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </SheetDescription>
+          <SheetDescription></SheetDescription>
         </SheetHeader>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Cálculos</AccordionTrigger>
+            <AccordionContent
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => router.push("/calculo/individual")}
+            >
+              Prazos Individuais
+              <Image src={Calculator} width={30} height={30} alt={""} />
+            </AccordionContent>
+            <AccordionContent
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => router.push("/calculo/lote")}
+            >
+              Prazos em Lote
+              <Image src={SheetIcon} width={30} height={30} alt={""} />
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Prazos</AccordionTrigger>
+            <AccordionContent
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => router.push("/prazos")}
+            >
+              Histórico.
+              <Image src={DeadlineIcon} width={30} height={30} alt={""} />
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Feriados</AccordionTrigger>
+            <AccordionContent
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => router.push("/feriados")}
+            >
+              Listagem e Criação.
+              <Image src={HolidayIcon} width={30} height={30} alt={""} />
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <SheetFooter className="w-full">
+          <SheetClose asChild>
+            <Button
+              type="submit"
+              className="w-full m-6"
+              onClick={() => router.push("/")}
+            >
+              Sair da Aplicação
+            </Button>
+          </SheetClose>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
