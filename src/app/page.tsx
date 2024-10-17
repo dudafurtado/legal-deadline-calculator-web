e2038d0aa37a2ff3293c3e307b6378465360c624
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-10-17 11:33:26 -0300

    fix: login page with logo and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,105 +1,17 @@
-"use client";
-
-import { DatePicker } from "@/components/DatePicker";
-import Menu from "@/components/Menu";
-import { SelectInput } from "@/components/SelectState";
+import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import PpIcon from "@/assets/pp-icon.webp";
 
 export default function Home() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
-
   return (
-    <article className="m-6">
-      <Menu />
-      <main className="flex flex-col items-center content-center">
-        <h1 className="text-2xl font-bold">Calcule aqui o seu prazo:</h1>
-        <Form {...form}>
-          <form className="space-y-8">
-            <FormField
-              // control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel>Data de início</FormLabel>
-                  <FormControl>
-                    {/* <Input placeholder="22/07/2030" {...field} /> */}
-                    <DatePicker />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              // control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel>Tipo de Prazo</FormLabel>
-                  <FormControl>
-                    <SelectInput />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              // control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel>Estado</FormLabel>
-                  <FormControl>
-                    <SelectInput />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              // control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel>Cidade</FormLabel>
-                  <FormControl>
-                    <SelectInput />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Calcular</Button>
-          </form>
-        </Form>
+    <article className="w-screen h-screen bg-black p-6">
+      <main className="flex flex-col items-center justify-center">
+        <Image src={PpIcon} alt={""} width={600} height={350} />
+        <p></p>
+        <Button variant="outline" className="w-1/2" asChild>
+          <Link href="/login">Login</Link>
+        </Button>
       </main>
     </article>
   );
