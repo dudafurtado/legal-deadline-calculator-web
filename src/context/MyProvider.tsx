b4709226e965569e2e899c6c43a276f086f7be9b
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-10-18 16:17:16 -0300

    ft: state cities added to context to list based on state

--- src/context/MyProvider.tsx
+++ src/context/MyProvider.tsx
@@ -3,17 +3,21 @@
 import { useState } from "react";
 import MyContext from "@/context/MyContext";
 import { MyProviderProps } from "@/interfaces/context";
-import { Holiday } from "@/interfaces/interfaces";
+import { Holiday } from "@/interfaces/entities";
+import { Cities } from "@/interfaces/entities";
 
 export default function MyProvider({ children }: MyProviderProps) {
   const [reloadHolidayList, setReloadHolidayList] = useState<boolean>(false);
   const [holidays, setHolidays] = useState<Holiday[]>([]);
+  const [cities, setCities] = useState<Cities[]>([]);
 
   const values = {
     reloadHolidayList,
     setReloadHolidayList,
     holidays,
     setHolidays,
+    cities,
+    setCities,
   };
 
   return <MyContext.Provider value={values}>{children}</MyContext.Provider>;
--- src/interfaces/context.tsx
+++ src/interfaces/context.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from "react";
-import { Holiday } from "@/interfaces/interfaces";
+import { Holiday } from "@/interfaces/entities";
+import { Cities } from "@/interfaces/entities";
 
 export interface MyContextType {
   reloadHolidayList: boolean;
   setReloadHolidayList: (term: boolean) => void;
   holidays: Holiday[];
   setHolidays: (term: Holiday[]) => void;
+  cities: Cities[];
+  setCities: (term: Cities[]) => void;
 }
 
 export interface MyProviderProps {
