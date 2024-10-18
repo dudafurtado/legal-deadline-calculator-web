2ecb5b8691998f2a2dac2258c3b3a4b83d910b52
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-10-18 16:18:28 -0300

    fix: indentation of code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ import PpIcon from "@/assets/pp-icon.webp";
 
 export default function Home() {
   return (
-    <article className="w-screen h-screen flex items-center justify-center bg-black p-6">
-      <main className="flex flex-col items-center justify-center text-white">
+    <article className="w-screen h-screen flex items-center justify-center bg-blueDark p-6">
+      <main className="flex flex-col items-center justify-center">
         <Image src={PpIcon} alt={""} width={400} height={300} />
-        <h1 className="text-2xl font-bold mb-10">Cálculadora de Prazos</h1>
+        <h1 className="text-2xl font-bold text-white mb-10 ">
+          Calculadora de Prazos
+        </h1>
         <Button
           variant="outline"
-          className="lg:w-4/12 md:w-5/12 sm:w-6/12 text-black"
+          className="lg:w-4/12 md:w-5/12 sm:w-6/12 font-semibold"
           asChild
         >
           <Link href="/login">Login</Link>
