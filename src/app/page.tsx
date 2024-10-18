1f2336d4a57f5e2768709e9d4736c3923e707d2f
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-10-18 09:49:04 -0300

    fix: login page align

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import PpIcon from "@/assets/pp-icon.webp";
 
 export default function Home() {
   return (
-    <article className="w-screen h-screen bg-black p-6">
-      <main className="flex flex-col items-center justify-center">
-        <Image src={PpIcon} alt={""} width={600} height={350} />
-        <p></p>
-        <Button variant="outline" className="w-1/2" asChild>
+    <article className="w-screen h-screen flex items-center justify-center bg-black p-6">
+      <main className="flex flex-col items-center justify-center text-white">
+        <Image src={PpIcon} alt={""} width={400} height={300} />
+        <h1 className="text-2xl font-bold mb-10">Cálculadora de Prazos</h1>
+        <Button
+          variant="outline"
+          className="lg:w-4/12 md:w-5/12 sm:w-6/12 text-black"
+          asChild
+        >
           <Link href="/login">Login</Link>
         </Button>
       </main>
